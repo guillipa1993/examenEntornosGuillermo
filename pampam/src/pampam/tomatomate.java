b811a5ef4d79a514d1b62d2cea2750131f932836
Paco Gomez <fjgomez@florida-uni.es>
2016-05-23 15:13:25 +0200

    TOMA QUE TOMA

--- pampam/src/pampam/tomatomate.java
+++ pampam/src/pampam/tomatomate.java
@@ -4,7 +4,7 @@ public class tomatomate {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for(int i=0;i<10000;i++) System.out.println("Guillermo me molas mucho");
 	}
 
 }
